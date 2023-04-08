fc8c30d5db612bbdcb80ba7a518bcb1fe0de1c57
Lewis Lin <linle211@gmail.com>
2023-04-07 19:53:47 -0700

    frontend component structure

--- client/client.js
+++ client/client.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Home from './home.js';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <h1>Begin Here</h1>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<h1>About</h1>}/>
+      <Route path="/portfolio" element={<h1>Portfolio</h1>}/>
+      <Route path="/contact" element={<h1>Contact</h1>}/>
+      <Route path="/blog" element={<h1>Blog</h1>}/>
+    </Routes>
   </BrowserRouter>
 );
---
+++ client/home.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+
+export default function Home() {
+  // return (<img src="./profilepicture.png" width={"150"} height={"150"}></img>)
+  return (<h1>Home</h1>)
+}
